ec719f31d186bd6f819945eb1d22eb61ceeb573f
Artur Milewski <artur.milewski@softwarehut.com>
2018-11-22 09:42:50 +0100

    improve PrintExample, IncremetDecrement and BigDecimalExample added

---
+++ src/BigDecimalExample.java
@@ -0,0 +1,19 @@
+import java.math.BigDecimal;
+
+public class BigDecimalExample {
+
+    // Nigdy nie liczymy pieniędzy na float czy double (Nie można poprawnie ująć wszystkich dziesiętnych ułamków w systemie binarnym)
+
+    public static void main(String[] args) {
+        double x = 0.02;
+        double y = 0.03;
+        double z = x - y;
+
+        System.out.println(z);
+        System.out.printf("%.2f\n" ,z);
+
+        BigDecimal a = new BigDecimal("0.02");
+        BigDecimal b = new BigDecimal("0.03");
+        System.out.println(a.subtract(b));
+    }
+}
---
+++ src/IncrementDecrement.java
@@ -0,0 +1,29 @@
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+        int x = 3;
+
+        System.out.printf("x: %d\n", x);
+        System.out.printf("x: %d\n", x);
+        System.out.printf("x++: %d\n", x++);
+        System.out.printf("x: %d\n", x);
+        System.out.printf("++x: %d\n", ++x);
+        System.out.printf("x: %d\n", x);
+
+        System.out.println();
+
+        int y;
+        y = x--;
+        System.out.printf("y = x--: %d\n", y);
+        System.out.printf("x: %d\n", x);
+        System.out.printf("y: %d\n", y);
+
+        System.out.println();
+
+        System.out.printf("x: %d\n", x);
+        y = --x;
+        System.out.printf("y = --x: %d\n", y);
+        System.out.printf("x: %d\n", x);
+        System.out.printf("++y: %d\n", ++y);
+    }
+}
