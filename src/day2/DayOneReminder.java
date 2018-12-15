52f72e77b3efaf0ffb43b331340052d3b95149c9
Artur <a.milewski.bia@gmail.com>
2018-12-15 18:22:01 +0100

    DayOneReminder added

---
+++ src/day2/DayOneReminder.java
@@ -0,0 +1,20 @@
+package day2;
+// Co wyświetli się w konsoli?
+public class DayOneReminder {
+    static double number = 10.99;
+
+    public static void main(String[] args) {
+        number = 102;
+        myMethod();
+        System.out.printf("number = %.1f", number++);
+    }
+
+    public static void myMethod() {
+        number = Math.PI;
+        mySecondMethod();
+    }
+
+    public static void mySecondMethod() {
+        number = 5.123;
+    }
+}
