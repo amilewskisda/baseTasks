0559deb736489be65b4acc50b046837e94a12bda
Artur <a.milewski.bia@gmail.com>
2018-12-18 18:54:37 +0100

    ForLoopChallange fix for 2d arrays

--- src/day2/ForLoopChallenge.java
+++ src/day2/ForLoopChallenge.java
@@ -1,25 +1,27 @@
 package day2;
 
+// https://stackoverflow.com/questions/4000169/getting-the-array-length-of-a-2d-array-in-java
+
 public class ForLoopChallenge {
     public static void main(String[] args) {
-        int[][] tab = new int[5][5];
+        int[][] tab = new int[5][8];
 
-/*        printDoubleTab(tab);
+        printDoubleTab(tab);
         System.out.println();
 
-        tab = new int[5][5];
+        tab = new int[5][8];
         tab = addOneRow(tab);
         printDoubleTab(tab);
         System.out.println();
 
-        tab = new int[5][5];
+        tab = new int[5][8];
         tab = addCrossRow(tab);
-        printDoubleTab(tab);*/
+        printDoubleTab(tab);
     }
 
     private static int[][] addOneRow(int[][] tab) {
         for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab.length; j++) {
+            for (int j = 0; j < tab[0].length; j++) {
                 if (i == 2) {
                     tab[i][j] = 1;
                 }
@@ -30,7 +32,7 @@ public class ForLoopChallenge {
 
     private static int[][] addCrossRow(int[][] tab) {
         for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab.length; j++) {
+            for (int j = 0; j < tab[0].length; j++) {
                 if (j == i) {
                     tab[i][j] = i;
                 }
@@ -41,7 +43,7 @@ public class ForLoopChallenge {
 
     private static void printDoubleTab(int[][] tab) {
         for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab.length; j++) {
+            for (int j = 0; j < tab[0].length; j++) {
                 System.out.print(tab[i][j] + " ");
             }
             System.out.println();
